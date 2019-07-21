dab44e10ae82fc85d2c788e92f3a6ced4cb3fd3c
jlmeister <iamphil4483@yahoo.com>
2019-07-21 04:43:56 -0500

    finished

--- src/radiocoffee/src/form.js
+++ src/radiocoffee/src/form.js
@@ -1,7 +1,6 @@
 function openForm() {
   document.getElementById("form").style.display = "block"
   document.getElementsByTagName("body")[0].style.position = "fixed"
-  console.log("hi there, you opened a form.")
 }
 function closeForm() {
   document.getElementById("form").style.display = "none"
