7639cf0bab1ebfad07c7931555610d3a1ecbce10
jlmeister <iamphil4483@yahoo.com>
2019-07-23 00:14:19 -0500

    styled, added reset button and removed the possibility to override previous moves.

--- src/tic-tac-toe/app.js
+++ src/tic-tac-toe/app.js
@@ -1,18 +1,7 @@
-// in html
-// create 3x3 grid
-
-// in JS
-// on click, insert 'O' or 'X' depending on previous click
-// var player1, var player2, var previousClick 
-// 
-
 var turnCount = 1
-const whoStarts = Math.floor(Math.random() * 10) % 2
+var whoStarts = Math.floor(Math.random() * 10) % 2
 console.log(whoStarts)
-// if (whoStarts == 0) {
-//   turnCount = 1
-// }
-// console.log(turnCount)
+
 function setXO() {
   if ((whoStarts + turnCount) % 2 == 1) {
     return 'X'
@@ -23,13 +12,14 @@ function setXO() {
 }
 
 function markXO(element) {
-  console.log('you clicked me', turnCount, element)
-  element.innerText = setXO()
-  
-  turnCount += 1
+  // console.log('you clicked me', turnCount, element)
+  if (element.innerHTML == "") {
+    element.innerHTML = setXO()
+    turnCount += 1
+  }
 }
 
-// function setXO(event) {
-//   console.log('you clicked me', event)
-//   event.target.innerText = 'O'
-// }
+// win condition:
+// array to hold values of each box
+// test horizontally, vertically, and diagonally for three in a row
+// declare winner is X, O, or a draw
