4cac420aea38681d5f23840a9ab2660daad10af2
jlmeister <iamphil4483@yahoo.com>
2019-07-26 20:07:00 -0500

    towers of hanoi. can move towers, but no win logic and no illegal move prevention.

--- src/hanoi/hanoi.js
+++ src/hanoi/hanoi.js
@@ -1,14 +1,22 @@
 'use strict';
 
 $(document).ready(function() {
-  let $block = null;
+  let $towerPiece = null;
   
   $('.tower').click(function() {
-    if ($block) {
-      $(this).append($block);
-      $block = null;
+    if ($towerPiece) {
+      $(this).append($($towerPiece)[0]);
+      //console.log('block', $towerPiece[0], 'jqblock', $($($towerPiece)[0]).width());
+      $towerPiece = null;
     } else {
-      $block = $(this).children().last().detach();
+      console.log('remove', $(this).children().last().width());
+      $towerPiece = $(this).children().last().detach();
     }
   })
 })
+
+// Prevent Move Logic:
+// check to see if you have a block
+// then check to see if your new target is empty.
+// if it is, then place the block. if not, check to see if its last child width is greater than what you're placing.
+// if it is, then place the block.
